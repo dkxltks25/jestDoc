69883f6da022205020e41f04abebaca74d80861c
bagjaehong <dkxltks25@naver.com>
2020-07-13 01:06:13 +0900

    adding positive number is not zero test

--- src/tests/matcher.test.js
+++ src/tests/matcher.test.js
@@ -4,3 +4,12 @@ test("object assignment", () => {
   data["tow"] = 2;
   expect(data).toEqual({ one: 1, tow: 2 });
 });
+
+// 특정 숫자가 있는지 판단 두수를 더해서 0되는 값이 있는가
+test("adding positive numbers is not zero", () => {
+  for (let a = 1; a < 10; a++) {
+    for (let b = 1; b < 10; b++) {
+      expect(a + b).not.toBe(0);
+    }
+  }
+});
